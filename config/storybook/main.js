51517cfdd5e9b97a74de7d492128d4492b1e56cd
Schultz <evan.schultz@molex.com>
2022-05-20 11:27:20 -0400

    Revert "add storybook"
    This reverts commit bebceb8ec32f4832a96c778102ecd371c0209dba.

--- config/storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-links']
-}
